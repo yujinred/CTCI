f4fe0a32b268ccd58624f5c25e5e0099e46ae978
Eugene Zhang <eugene.zhang8@gmail.com>
2020-03-05 00:40:41 -0500

    Fixing the Test code to use Junit instead of hacked solution

--- test/com/company/ctci/chapter1/TestArraysAndStrings.java
+++ test/com/company/ctci/chapter1/TestArraysAndStrings.java
@@ -1,74 +1,38 @@
 package com.company.ctci.chapter1;
 
-import com.company.ctci.TestUtil;
+import org.junit.jupiter.api.Test;
 
-public class TestArraysAndStrings {
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestArraysAndStrings {
 
     private static final ArraysAndStrings solution = new ArraysAndStrings();
 
-    private static void testIsAnagram() throws Exception {
-        TestUtil.assertEquals("Left is empty",
-                solution.isAnagram("", "abc"),
-                false);
-
-        TestUtil.assertEquals("Right is empty",
-                solution.isAnagram("abc", ""),
-                false);
-
-        TestUtil.assertEquals("Actual test this time",
-                solution.isAnagram("cba", "abc"),
-                true);
-
-        TestUtil.assertEquals("Cool anagram",
-                solution.isAnagram("admirer", "married"),
-                true);
+    @Test
+    void testIsAnagram() {
+        assertFalse(solution.isAnagram("", "abc"));
+        assertFalse(solution.isAnagram("abc", ""));
+        assertTrue(solution.isAnagram("cba", "abc"));
+        assertTrue(solution.isAnagram("admirer", "married"));
     }
 
-    private static void testRemoveDuplicates() throws Exception {
-        TestUtil.assertEquals("it works!",
-                solution.removeDuplicates("aaabbccd"),
-                "abcd");
-
-        TestUtil.assertEquals("it works for strings unordered!",
-                solution.removeDuplicates("zqsdczpos"),
-                "zqsdcpo");
+    @Test
+    void testRemoveDuplicates() {
+        assertEquals("abcd", solution.removeDuplicates("aaabbccd"));
+        assertEquals("zqsdcpo", solution.removeDuplicates("zqsdczpos"));
     }
 
-    private static void testReverseString() throws Exception {
-        TestUtil.assertEquals("Should reverse the string",
-                solution.reverseString("Dog"),
-                "goD");
-        TestUtil.assertEquals("Empty String",
-                solution.reverseString(""),
-                "");
-        TestUtil.assertEquals("Reverse this string",
-                solution.reverseString("wow this is a long string"),
-                "gnirts gnol a si siht wow");
+    @Test
+    void testReverseString() {
+        assertEquals("goD", solution.reverseString("Dog"));
+        assertEquals("", solution.reverseString(""));
+        assertEquals("gnirts gnol a si siht wow", solution.reverseString("wow this is a long string"));
     }
 
-    private static void testUniqueChar() throws Exception {
-        TestUtil.assertEquals("All letters should be unique",
-                solution.hasAllUniqueCharacters("asdfg"),
-                true
-                );
-        TestUtil.assertEquals("Duplicate letter should be detected",
-                solution.hasAllUniqueCharacters("asdfa"),
-                false
-        );
-        TestUtil.assertEquals("Empty String",
-                solution.hasAllUniqueCharacters(""),
-                true
-        );
-    }
-
-    public static void main(String[] args) {
-        try {
-            testUniqueChar();
-            testReverseString();
-            testRemoveDuplicates();
-            testIsAnagram();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Test
+    void testUniqueChar() {
+        assertTrue(solution.hasAllUniqueCharacters("asdfg"));
+        assertFalse(solution.hasAllUniqueCharacters("asdfa"));
+        assertTrue(solution.hasAllUniqueCharacters(""));
     }
 }
